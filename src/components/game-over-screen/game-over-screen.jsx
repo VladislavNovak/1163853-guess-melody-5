86242141f1585bf64bf9c838c97e7d50e70126f5
Valdix <novaldix@gmail.com>
2020-10-27 00:02:03 +0300

    Рефакторит действия. Другой взгляд на паттерн «Action creator»

--- src/components/game-over-screen/game-over-screen.jsx
+++ src/components/game-over-screen/game-over-screen.jsx
@@ -2,9 +2,9 @@
 import React from "react";
 import PropTypes from "prop-types";
 import {connect} from "react-redux";
-import {ActionCreator} from "../../store/action";
+import {resetGame} from "../../store/action";
 
-const GameOverScreen = ({onReplayButtonClick, resetGame}) => {
+const GameOverScreen = ({onReplayButtonClick, resetGameAction}) => {
   return (
     <section className="result">
       <div className="result__logo">
@@ -14,7 +14,7 @@ const GameOverScreen = ({onReplayButtonClick, resetGame}) => {
       <p className="result__total result__total--fail">У вас закончились все попытки. Ничего, повезёт в следующий раз!</p>
       <button
         onClick={() => {
-          resetGame();
+          resetGameAction();
           onReplayButtonClick();
         }}
         className="replay"
@@ -28,12 +28,12 @@ const GameOverScreen = ({onReplayButtonClick, resetGame}) => {
 
 GameOverScreen.propTypes = {
   onReplayButtonClick: PropTypes.func.isRequired,
-  resetGame: PropTypes.func.isRequired,
+  resetGameAction: PropTypes.func.isRequired,
 };
 
 const mapDispatchToProps = (dispatch) => ({
-  resetGame() {
-    dispatch(ActionCreator.resetGame());
+  resetGameAction() {
+    dispatch(resetGame());
   },
 });
 
